c01dee226d75a77fde0a8fe819cb562f2ff476bc
tiger7878 <zhcw63@163.com>
2018-10-05 17:22:31 +0800

    RestController和RequestMapping简单示例

---
+++ monkey-security-demo/src/main/java/com/monkey/web/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.monkey.web.controller;
+
+import com.monkey.dto.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: monkey
+ * @date: 2018/10/5 17:14
+ */
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public List<User> query(){
+        List<User> users=new ArrayList<User>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+
+        return users;
+    }
+
+}
