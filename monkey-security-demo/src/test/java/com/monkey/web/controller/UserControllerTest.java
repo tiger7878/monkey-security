10c3f6d03ccaf3c5bbbd51fbf8a5d26406692a0e
tiger7878 <zhcw63@163.com>
2018-10-05 17:12:55 +0800

    ResutfullAPI的简单的测试用例

---
+++ monkey-security-demo/src/test/java/com/monkey/web/controller/UserControllerTest.java
@@ -0,0 +1,44 @@
+package com.monkey.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author: monkey
+ * @date: 2018/10/5 16:38
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        mockMvc= MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void whenQuerySuccess() throws Exception {
+        mockMvc.perform(get("/user") //get请求/user资源
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk()) //期望响应码200
+                .andExpect(jsonPath("$.length()").value(3)); //期望json集合中有3个元素
+    }
+
+}
