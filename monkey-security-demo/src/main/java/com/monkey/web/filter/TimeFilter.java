826c226d4cd87ac39532ad60d7190e26b73c0ecd
tiger7878 <zhcw63@163.com>
2018-10-06 15:39:25 +0800

    自定义Filter

---
+++ monkey-security-demo/src/main/java/com/monkey/web/filter/TimeFilter.java
@@ -0,0 +1,38 @@
+package com.monkey.web.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * 自定义Filter，记得标注为组件
+ * @author: monkey
+ * @date: 2018/10/6 15:33
+ */
+@Component
+public class TimeFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("time filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        Long startTime=new Date().getTime();
+
+        filterChain.doFilter(servletRequest,servletResponse);//放行
+
+        System.out.println("请求本方法耗时："+((new Date().getTime())-startTime)+" 毫秒！");
+
+        System.out.println("time filter doFilter");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("time filter destory");
+    }
+
+}
